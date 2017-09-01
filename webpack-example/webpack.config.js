f2eeb9fdd41cc8b9f0257f5cb6bcf0c8de3b39e1
SimoneAlbertini <albertini.simone@gmail.com>
2017-09-01 18:22:24 +0200

    semicolumn missing

--- webpack-example/webpack.config.js
+++ webpack-example/webpack.config.js
@@ -3,7 +3,7 @@ var HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
   template: __dirname + '/app/index.html',
   filename: 'index.html',
   inject: 'body'
-})
+});
 
 module.exports = {
   context: __dirname + "/app",
